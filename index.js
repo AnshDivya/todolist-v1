186890a6b567ba41d702da7e7c0511a5b75994c3
AnshDivya <divyansh15.dt@gmail.com>
2020-02-04 22:04:40 +0530

    Add form to take the task input

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 app.listen(3000, function() {
   console.log("Server started on port number 3000");
 });
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+
 app.get("/", function(req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = weekday[currentDay];
+
+  var options = {
+    weekday: "short",
+    day: "numeric",
+    month: "long"
+  }
+  var day = today.toLocaleDateString("en-US", options);
   res.render("list", {kindOfDay: day});
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body.task);
 });
