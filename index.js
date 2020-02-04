eb13a59046127535b921853a642d98cf33fd87c9
AnshDivya <divyansh15.dt@gmail.com>
2020-02-04 23:00:13 +0530

    Add new task functionality i.e., the user entered names will be added in the unordered list

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+var tasks = ["Buy Food","Cook Food","Eat Food"];
+
 app.listen(3000, function() {
   console.log("Server started on port number 3000");
 });
@@ -18,10 +21,14 @@ app.get("/", function(req, res) {
     day: "numeric",
     month: "long"
   }
+
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newTasks: tasks});
 });
 
 app.post("/", function(req, res) {
-  console.log(req.body.task);
+  var newTask = req.body.task;
+  tasks.push(newTask);
+
+  res.redirect("/");
 });
