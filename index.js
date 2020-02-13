301cbc8cd78365f64e704fae8d487a35cceb0ceb
AnshDivya <divyansh15.dt@gmail.com>
2020-02-13 11:16:48 +0530

    Add footer.ejs and header.js as template for the pages. Also add self made module for date

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
+console.log(date);
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-var tasks = ["Buy Food","Cook Food","Eat Food"];
-
+const tasks = ["Buy Food","Cook Food","Eat Food"];
+const workTasks = [];
 app.listen(3000, function() {
   console.log("Server started on port number 3000");
 });
 
 app.get("/", function(req, res) {
-  var today = new Date();
 
-  var options = {
-    weekday: "short",
-    day: "numeric",
-    month: "long"
-  }
-
-  var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day, newTasks: tasks});
+  const day = date.getDate();
+  res.render("list", {kindOfDay: day, newTasks: tasks, actionRoute: "/"});
 });
 
 app.post("/", function(req, res) {
-  var newTask = req.body.task;
+  const newTask = req.body.task;
   tasks.push(newTask);
 
   res.redirect("/");
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    kindOfDay: "Work",
+    newTasks: workTasks,
+    actionRoute: "/work"
+  })
+});
+
+app.post("/work", function(req, res) {
+  const newTask = req.body.task;
+  workTasks.push(newTask);
+
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
